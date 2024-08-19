6f413a2e654acee199f7bafe40ed17403faec3a7
4est.lil.dev <carneo.dev@gmail.com>
2024-08-20 03:10:03 +0900

    Implement Main entry point
    실행될 메인 로직을 작성하고 정렬 알고리즘 사용

---
+++ src/main/java/org/fastcam/javaoop/Main.java
@@ -0,0 +1,16 @@
+package org.fastcam.javaoop;
+
+import org.fastcam.javaoop.logic.BubbleSort;
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort<String> sort = new BubbleSort<>();
+
+        System.out.println("[result] " + sort.sort(Arrays.asList(args)));
+
+    }
+}
